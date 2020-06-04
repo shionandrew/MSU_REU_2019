b26d97e1e7e77d835fedd9afc45ffa66eb0e4766
shionandrew <47239605+shionandrew@users.noreply.github.com>
2020-06-04 14:31:15 -0700

    Add files via upload

--- PlotsForPaper.py
+++ PlotsForPaper.py
@@ -33,6 +33,7 @@ def plotLightCurve():
     magerr = []
     mag = []
     abjd = []
+    #with open('/Users/touatokuchi/Desktop/MSU/PlotsForPaper/LightCurveVariable1.csv') as csvfile:
     with open('/Users/touatokuchi/Desktop/MSU/PlotsForPaper/StripeVariable.csv') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
@@ -44,26 +45,32 @@ def plotLightCurve():
                 print("error")
 
     P = 1.0750575
-    t0 = 53505.41686
+    #P = 1.2996582
+    t0 = 53611.39860
 
     aphase = []
     for i in range(len(abjd)):
         aphase.append(foldAt(abjd[i],P,T0=t0,getEpoch=False))
-    aphase.reverse()
-    aphase2 = aphase + aphase
-    magerr.reverse()
-    magerr2 = magerr + magerr
-    mag.reverse()
-    mag2 = mag+mag
+
+    mag2 = []
+    aphase2 = []
+    magerr2 = []
+    for i in range(len(aphase)):
+        mag2.append(mag[i])
+        magerr2.append(magerr[i])
+        aphase2.append(aphase[i]+1)
+    mag = mag + mag2
+    aphase = aphase + aphase2
+    magerr = magerr + magerr2
 
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
     plt.ylabel(r'\textbf{Magnitude}')
     plt.xlabel(r'\textbf{Phase}')
-    plt.gca().invert_xaxis()
-    plt.ylim(17, 18)
-    plt.scatter(aphase2, mag2, color = 'red', s = .5)
-    plt.errorbar(aphase2, mag2, yerr=magerr2, ecolor = 'black', lw = .6, capsize = 1, fmt = 'none')
+    plt.gca().invert_yaxis()
+    plt.ylim(17.8, 17.1)
+    plt.scatter(aphase, mag, color = 'red', s = .5)
+    plt.errorbar(aphase, mag, yerr=magerr, ecolor = 'black', lw = .6, capsize = 1, fmt = 'none')
     plt.show()
 
 def foldAt(time, period, T0=0.0, getEpoch=False):
